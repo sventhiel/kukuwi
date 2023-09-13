b37dfd07c94f7946eb155923f8953621b8cafdef
David Schöne <david.blaa@googlemail.com>
2023-09-13 09:17:50 +0200

    add files

---
+++ src/routes/Level1/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+import InstrumentCards from "$lib/components/InstrumentCards.svelte";
+import { instruments } from "$lib/data/data"
+function selectFn(e)
+{
+    alert(e.detail);
+    var selectedId = e.detail;
+}
+
+ 
+</script>
+
+<h1>Level 0</h1>
+<InstrumentCards {instruments} gameMode={true} on:select={selectFn}/>
