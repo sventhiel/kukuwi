7879bada2449fdafc510d7719f184ec83ac40005
David Schöne <david.blaa@googlemail.com>
2023-09-13 15:02:52 +0200

    add game options

--- src/routes/Level1/+page.svelte
+++ src/routes/Level1/+page.svelte
@@ -1,15 +1,21 @@
 <script>
 import InstrumentCards from "$lib/components/InstrumentCards.svelte";
 import { instruments } from "$lib/data/data"
+import GameOptions from "./GameOptions.svelte";
 
+
+let selectedId=0;
+let playId=0;
 
 function selectFn(e)
 {
-    alert(e.detail);
-    var selectedId = e.detail;
+
+    selectedId = e.detail;
+    console.log("result", selectedId, playId)
 }
 
  
 </script>
 
 <InstrumentCards {instruments} gameMode={true} on:select={selectFn}/>
+<GameOptions {instruments} bind:playId />
